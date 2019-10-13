80274ab99dd389105a8dcfd5126f174cc6e9dadd
uehr <uehara.yudai@gmail.com>
2019-10-13 21:38:44 +0900

    解析WEBAPIをroute

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "analysis_tw_user#index"
-  get "/analysis/:id", to: "analysis_tw_user#show"
+  get "/analysis/:id", to: "analysis_tw_user#new", as: "analysis"
 end
