f40fc9498a78625c32c3409cf64eb097a61d0ed9
uehr <uehara.yudai@gmail.com>
2019-10-14 20:14:13 +0900

    エラーハンドリングを実装

--- app/controllers/analysis_tw_user_controller.rb
+++ app/controllers/analysis_tw_user_controller.rb
@@ -5,8 +5,19 @@ class AnalysisTwUserController < ApplicationController
     end
 
     def new
-        analyzer = AnalysisTwUser.new(params[:id])
-        ranking = analyzer.tweet_words_ranking
-        render :json => ranking
+        begin
+            analyzer = AnalysisTwUser.new(params[:id])
+            ranking = analyzer.tweet_words_ranking
+            render :json => ranking
+        rescue Twitter::Error::Unauthorized, Twitter::Error::NotFound
+            msg = "不正なユーザーIDです"
+            flash[:danger] = msg
+            render status: 400, json: {error: msg}
+        rescue => err
+            puts err
+            msg = "サーバーエラー"
+            flash[:danger] = msg
+            render status: 500, json: {error: msg}
+        end
     end
 end
