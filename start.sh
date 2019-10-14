9149981c05022ce01afd0987475fdf2c57462973
uehr <uehara.yudai@gmail.com>
2019-10-14 10:31:07 +0900

    バックグランドで辞書をダウンロード

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 # neologd辞書をダウンロード
-wget https://uehr-service.s3-ap-northeast-1.amazonaws.com/mecab-dict.tar.xz
-tar -xf mecab-dict.tar.xz
+wget https://uehr-service.s3-ap-northeast-1.amazonaws.com/mecab-dict.tar.xz && tar -xf mecab-dict.tar.xz &
 
 # デフォルトでneologd辞書を使用
 sed -i -e 's/^dicdir/dicdir = \/app\/mecab-dict\n; dicdir/g' ${MECABRC}
