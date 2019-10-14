2bba364b323b5c240c8a938ded87099d03aa7e9e
uehr <uehara.yudai@gmail.com>
2019-10-14 10:33:43 +0900

    wgetのログを非表示

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # neologd辞書をダウンロード
-wget https://uehr-service.s3-ap-northeast-1.amazonaws.com/mecab-dict.tar.xz && tar -xf mecab-dict.tar.xz &
+wget -q https://uehr-service.s3-ap-northeast-1.amazonaws.com/mecab-dict.tar.xz && tar -xf mecab-dict.tar.xz &
 
 # デフォルトでneologd辞書を使用
 sed -i -e 's/^dicdir/dicdir = \/app\/mecab-dict\n; dicdir/g' ${MECABRC}
