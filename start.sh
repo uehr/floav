07f87b59d001355e0ec75c944696618e2008984b
uehr <uehara.yudai@gmail.com>
2019-10-14 09:42:11 +0900

    start.sh内で辞書をインストール

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 # デフォルトでneologd辞書を使用
-sed -i -e 's/^dicdir/dicdir = \/app\/mecab-ipadic-neologd\/sys.dic\n; dicdir/g' ${MECABRC}
+sed -i -e 's/^dicdir/dicdir = \/app\/mecab-ipadic-neologd\n; dicdir/g' ${MECABRC}
+
+# mecabのPATH設定
+export PATH="/app/vendor/mecab/bin:$PATH"
+
+# neologd辞書をインストール
+git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+cd mecab-ipadic-neologd
+./bin/install-mecab-ipadic-neologd --ignore_adverb --ignore_noun_sahen_conn_ortho --ignore_adjective_std --ignore_adjective_verb --ignore_ill_formed_words --ignore_noun_sahen_conn_ortho
+
 bundle exec rackup config.ru -p $PORT
