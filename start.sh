e4e761fd52fb23a80ac305d6fe6fc0c75d55cee1
uehr <uehara.yudai@gmail.com>
2019-10-14 10:14:20 +0900

    s3上に辞書を配置

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
+# neologd辞書をダウンロード
+wget https://uehr-service.s3-ap-northeast-1.amazonaws.com/mecab-dict.tar.xz
+tar -xf mecab-dict.tar.xz
+
 # デフォルトでneologd辞書を使用
-sed -i -e 's/^dicdir/dicdir = \/app\/mecab-ipadic-neologd\n; dicdir/g' ${MECABRC}
-
-# mecabのPATH設定
-export PATH="/app/vendor/mecab/bin:$PATH"
-
-# neologd辞書をインストール
-git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
-cd mecab-ipadic-neologd
-./bin/install-mecab-ipadic-neologd --ignore_adverb --ignore_noun_sahen_conn_ortho --ignore_adjective_std --ignore_adjective_verb --ignore_ill_formed_words --ignore_noun_sahen_conn_ortho
+sed -i -e 's/^dicdir/dicdir = \/app\/mecab-dict\n; dicdir/g' ${MECABRC}
 
 bundle exec rackup config.ru -p $PORT
