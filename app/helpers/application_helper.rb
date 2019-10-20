58f075a46e4e89a4021b4270af0ddfc9b2066a6b
uehr <uehara.yudai@gmail.com>
2019-10-20 19:30:52 +0900

    OGPを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+    def default_meta_tags
+        {
+        title:       "Floav",
+        description: "ツイートから関心のあるワードを抽出",
+        noindex: ! Rails.env.production?,
+        charset: "UTF-8",
+        og: {
+            title: :title,
+            description: :description,
+            type: "website",
+            url: request.original_url,
+            image: image_url("wordcloud.png"),
+            site_name: "Floav",
+            locale: "ja_JP"
+        },
+        twitter: {
+            card: 'summary',
+            image: image_url("wordcloud.png")
+        }
+        }
+    end
 end
